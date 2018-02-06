b94816149e1092cb4c7fee86a3b0fac576711b5e
iMATCH <imatch@iMATCHs-Mac.local>
2018-02-06 10:02:28 -0800

    Initial Commit

---
+++ ALILON ZTests/ALILON_ZTests.swift
@@ -0,0 +1,36 @@
+//
+//  ALILON_ZTests.swift
+//  ALILON ZTests
+//
+//  Created by iMATCH on 2/6/18.
+//  Copyright © 2018 iMATCH. All rights reserved.
+//
+
+import XCTest
+@testable import ALILON_Z
+
+class ALILON_ZTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
